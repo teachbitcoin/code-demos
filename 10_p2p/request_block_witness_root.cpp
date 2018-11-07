a848fb2e3f2afbd11a16fe9e07fcfc0522cfd706
James Chiang <james.chiangwu@gmail.com>
2018-11-07 10:02:55 +0100

    various

---
+++ 10_p2p/request_block_witness_root.cpp
@@ -0,0 +1,125 @@
+// Compile with:
+// g++ -std=c++11 `pkg-config --cflags libbitcoin libbitcoin-network --libs libbitcoin libbitcoin-network` -o channel_subscription_getblock_example channel_subscription_getblock_example.cpp
+
+#include <bitcoin/network.hpp>
+
+BC_USE_LIBBITCOIN_MAIN
+
+int bc::main(int argc, char* argv[])
+{
+    // Create P2P object.
+    // -------------------------------------------------------------------------
+
+    // Single Outbound Channel.
+    bc::network::settings settings(bc::config::settings::mainnet);
+    settings.seeds.clear();
+    settings.seeds.push_back({"mainnet1.libbitcoin.net",8333});
+    settings.seeds.push_back({"mainnet2.libbitcoin.net",8333});
+    settings.seeds.push_back({"mainnet3.libbitcoin.net",8333});
+    settings.inbound_connections = 0; // default setting = 0.
+    settings.outbound_connections = 1;  // default setting = 8.
+    settings.host_pool_capacity = 100; // default setting = 0.
+    settings.threads = 3; // default setting = 0.
+    settings.services = bc::message::version::service::none; // default none.
+
+    // Create P2P object.
+    bc::network::p2p my_p2p(settings);
+
+    // Promise which signals completion to main loop (below).
+    std::promise<bc::code> promise;
+
+    // Handlers
+    // -------------------------------------------------------------------------
+
+    const auto send_handler = [](const bc::code& ec)
+        {
+            bc::cout << std::string("[My App] GetData message for block sent: ")
+                .append(ec.message()) << std::endl;
+        };
+
+    const auto block_subscription_handler = [&promise](const bc::code& ec,
+        bc::message::block::const_ptr block)
+    {
+        if (ec)
+            return true;
+
+        // ---------------------------------------------------------------------
+        // Derive required txid, witness root, witness data here.
+        // ---------------------------------------------------------------------     
+        
+        promise.set_value(ec);
+
+        return false;
+        // Note: It is not possible to a message to request further headers
+        // from the peer from this subscription_handler, since no channel
+        // pointer is accessible.
+    };
+
+    const auto connect_handler = [&block_subscription_handler, &send_handler](
+        const bc::code& ec, bc::network::channel::ptr channel)
+    {
+        // Resubscribe if connect error occurs.
+        if (ec)
+            return true;
+
+        // Get negotiated version.
+        bc::cout << std::string("[My App] New channel with version: ")
+            .append(std::to_string(channel->peer_version()->value()))
+            << std::endl;
+
+        // Depending on version, get_blocks or get_headers.
+        if (channel->peer_version()->value() >=
+            bc::message::version::level::headers)
+        {
+            // Subscribe to block message.
+            channel->subscribe<bc::message::block>(
+                  block_subscription_handler);
+
+            // Send get_data(inv(block_type, block_hash)).
+            const auto type =
+                bc::message::inventory_vector::type_id::witness_block;
+            const auto block_hash = bc::hash_literal(
+                "000000000000000000018b63b42f45010822ed77ba6000a5d193283ff812c274");
+            bc::message::inventory_vector inv_vector(type, block_hash);
+            bc::message::get_data get_data_msg({inv_vector});
+
+            channel->send(get_data_msg, send_handler);
+        }
+
+        else
+        {
+            // We reject peer that do not support headers first sync.
+            channel->stop(bc::error::channel_stopped);
+        }
+
+        // Resubscribe
+        return true;
+    };
+
+    const auto run_handler = [&my_p2p](const bc::code& ec)
+    {
+        // Omitted.
+    };
+
+    const auto start_handler = [&my_p2p, &promise, &connect_handler,
+        &run_handler](const bc::code& ec)
+    {
+        if (ec && ec != bc::error::peer_throttling)
+        {
+            promise.set_value(ec);
+        }
+
+        // Network run.
+        my_p2p.subscribe_connection(connect_handler);
+        my_p2p.run(run_handler);
+    };
+
+    // Start P2P object.
+    // -------------------------------------------------------------------------
+
+    my_p2p.start(start_handler);
+
+    std::cout << promise.get_future().get().message() << std::endl;
+
+    return 0;
+}
---
+++ setup/AWS_linux2_setup.sh
@@ -0,0 +1,29 @@
+# Script for building toolkit for teachbitcoin code-demos and exercises.
+# Only for AWS linux 2.
+
+# Build BX
+# Build dependencies
+# sudo yum install autoconf automake libtool pkgconfig boost-devel
+# sudo yum groupinstall "Development Tools"
+# wget https://raw.githubusercontent.com/libbitcoin/libbitcoin-explorer/version3/install.sh
+# sudo chmod +x install.sh
+# sudo ./install.sh
+
+# Download BX binary
+wget https://github.com/libbitcoin/libbitcoin-explorer/releases/download/v3.2.0/bx-linux-x64-qrcode
+sudo chmod +x bx-linux-x64-qrcode
+sudo cp bx-linux-x64-qrcode /usr/local/bin/bx
+sudo cp bx_config_files/bx_testnet.cfg /usr/local/etc/libbitcoin/bx_testnet.cfg
+sudo cp bx_config_files/bx_mainnet.cfg /usr/local/etc/libbitcoin/bx_mainnet.cfg
+sudo cp bx_config_files/bx.cfg /usr/local/etc/libbitcoin/bx.cfg
+echo "export BX_CONFIG='/usr/local/etc/libbitcoin/bx.cfg'" >> $HOME/.bash_profile
+
+#Install miniconda, jupyter, cling kernel, bash kernel.
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sudo chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+
+conda install xeus-cling notebook -c QuantStack -c conda-forge
+
+pip install bash_kernel
+python -m bash_kernel.install
