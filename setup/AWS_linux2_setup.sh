ec62fd1d2f84f20823edb09fc6bc9ee3be7c9af6
James Chiang <james.chiangwu@gmail.com>
2019-03-03 11:46:53 +0100

    setup script for AWS workspace. Conda3 -> Conda2

--- setup/AWS_linux2_setup.sh
+++ setup/AWS_linux2_setup.sh
@@ -19,13 +19,11 @@ sudo cp bx_config_files/bx_mainnet.cfg /usr/local/etc/libbitcoin/bx_mainnet.cfg
 sudo cp bx_config_files/bx.cfg /usr/local/etc/libbitcoin/bx.cfg
 echo 'export BX_CONFIG="/usr/local/etc/libbitcoin/bx.cfg"' >> $HOME/.bashrc
 
-#Install miniconda, jupyter, cling kernel, bash kernel.
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sudo chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh
+#Install miniconda, jupyter, bash kernel.
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+sudo chmod +x Miniconda2-latest-Linux-x86_64.sh
+./Miniconda2-latest-Linux-x86_64.sh
 source $HOME/.bashrc
-
-conda install notebook
 
 pip install bash_kernel --user
 python -m bash_kernel.install
